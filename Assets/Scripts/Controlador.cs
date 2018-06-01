eac1e39f26be6a73632bfd149e2569a33bd34734
ItaloFrancoSulca <italo.franco.16@hotmail.com>
2018-06-01 13:08:50 -0500

    Modelo

--- Assets/Scripts/Controlador.cs
+++ Assets/Scripts/Controlador.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class Controlador : MonoBehaviour {
 
+
+    public string[] text;
+
 	// Use this for initialization
 	void Start () {
-		
+        text[0] = "Hola soy Guppu...";
+        text[1] = "...Y esta es mi historia";
+        text[2] = "Yo solía vivir en la Gran Mordistrón";
+        text[3] = "La Gran Mordistrón queda cerca al Árbol de Uchucuta";
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log (text[0]);
+        }
+
 	}
 }
