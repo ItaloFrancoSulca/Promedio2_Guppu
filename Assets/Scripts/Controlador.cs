80c14dbb010b38b441da35bfda6eab8d0f34c691
ItaloFrancoSulca <38414518+ItaloFrancoSulca@users.noreply.github.com>
2018-06-01 14:12:48 -0500

    I think Last Commit

--- Assets/Scripts/Controlador.cs
+++ Assets/Scripts/Controlador.cs
@@ -1,26 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Controlador : MonoBehaviour {
+public class Controlador : MonoBehaviour
+{
 
 
     public string[] text;
+    Text firstTextComponent;
+    Text secondTextComponent;
+    Text thirdTextComponent;
+    Text lastTextComponent;
+    public GameObject[] theCanvas;
 
-	// Use this for initialization
-	void Start () {
+    private void Awake()
+    {
+        firstTextComponent = theCanvas[0].GetComponent<Text>();
+        secondTextComponent = theCanvas[1].GetComponent<Text>();
+        thirdTextComponent = theCanvas[2].GetComponent<Text>();
+        lastTextComponent = theCanvas[3].GetComponent<Text>();
+    }
+    void Start()
+    {
         text[0] = "Hola soy Guppu...";
         text[1] = "...Y esta es mi historia";
         text[2] = "Yo solía vivir en la Gran Mordistrón";
         text[3] = "La Gran Mordistrón queda cerca al Árbol de Uchucuta";
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.A))
-        {
-            Debug.Log (text[0]);
-        }
 
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        firstTextComponent.text = text[0];
+        secondTextComponent.text = text[1];
+        thirdTextComponent.text = text[2];
+        lastTextComponent.text = text[3];
+
+
+    }
 }
