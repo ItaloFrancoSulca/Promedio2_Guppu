5e2c6e3da8050adaaa3a9fb7c3b832da9977cb5d
JoseAlcas <jose.ach.22@outlook.com>
2018-06-01 13:59:48 -0500

    Se añadio un Menu principal

---
+++ Assets/Scripts/Control.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Control : MonoBehaviour {
+
+	public void CambiarEscena(string nombre)
+	{
+      print("Cambiando a la escena" + nombre);
+	  SceneManager.LoadScene(nombre);
+	}
+
+}
